eb9341e65275061cc33b277c3c45cee4d4ea0f4d
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2022-05-11 09:14:42 +0200

    Needed nuget package

--- InternshipAPI/Startup.cs
+++ InternshipAPI/Startup.cs
@@ -29,8 +29,8 @@ namespace InternshipAPI
         {
 
             services.AddControllers();
-            //services.AddDbContext<PersonContext>(opt => opt.UseSqlServer(@"Server=DESKTOP-8NVTJM1\SQLEXPRESS; Database=HovedOpgave; Trusted_Connection=True;MultipleActiveResultSets=true"));
-            services.AddDbContext<PersonContext>(options => options.UseMySQL("Server=localhost; Database=HovedOpgave; User=root; Password=Padgok-mizdok-4cakbe"));
+            services.AddDbContext<PersonContext>(opt => opt.UseSqlServer(@"Server=DESKTOP-8NVTJM1\SQLEXPRESS; Database=HovedOpgave; Trusted_Connection=True;MultipleActiveResultSets=true"));
+            //services.AddDbContext<PersonContext>(options => options.UseMySQL("Server=localhost; Database=HovedOpgave; User=root; Password=Padgok-mizdok-4cakbe"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "InternshipAPI", Version = "v1" });
