e4c2f29812643cb52c9ca0e95a271c6439d3ee52
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2022-05-20 13:51:43 +0200

    Fixed delete in PersonManager

--- InternshipAPI/Manager/PersonManager.cs
+++ InternshipAPI/Manager/PersonManager.cs
@@ -77,6 +77,13 @@ namespace InternshipAPI.Manager
         {
             Person person = _context.Person.Find(id);
             if (person == null) return null;
+            List<Status> statuses = new List<Status>
+                    {
+                        new Status{PersonId = id, MyStatus ="I am attending"},
+                        new Status{PersonId = id, MyStatus ="I am not attending"},
+                        new Status{PersonId = id, MyStatus ="I have not answered"}
+                    };
+            _context.RemoveRange(statuses);
             _context.Person.Remove(person);
             _context.SaveChanges();
             return person;
